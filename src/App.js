8d231b25aca45ab0fbc5cd58cbe757572e58cc79
michael soto <michael_nickolas@hotmail.com>
2022-05-16 13:02:43 -0300

    Organização de pastas
    Michael Soto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { theme } from "./constants/theme";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import Router from "./routes/router";
+
+//TODO  FIZ BASEADO NO 4Food,SINTAM-SE A VONTADE PARA MUDAR ALGO QUE ESTA FALTANDO OU QUE PRECISE DE ALTERAÇÃO
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router />
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/HomeScreenPage/HomeScreenPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HomeScreenPage() {
+  return <div>HomeScreenPage</div>;
+}
